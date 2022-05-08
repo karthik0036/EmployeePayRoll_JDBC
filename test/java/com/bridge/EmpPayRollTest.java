75966fbd0e40f4368d8ea113f1bba0eca52d2d22
Karthik <jeefkarthik036@gmail.com>
2022-05-08 12:12:09 +0530

    Added Test Cases

---
+++ test/java/com/bridge/EmpPayRollTest.java
@@ -0,0 +1,24 @@
+package com.bridge;
+
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.List;
+
+public class EmpPayRollTest {
+    EmpPayRollService employeePayRollService = new EmpPayRollService();
+
+    @Test
+    public void givenEmployeePayrollDB_WhenRetrieved_ShouldMatchEmployeeCount() {
+        String sql = "select * from employee_payroll";
+        List<EmpInfo> employeePayrollDataList = employeePayRollService.queryExecute(sql);
+        Assert.assertEquals(7, employeePayrollDataList.size());
+    }
+
+    @Test
+    public void givenUpdatingCersiBasicPay_whenUpdate_ShouldReturnUpdatedPay() {
+        double BasicPay = 30000;
+        String Name = "Cersi";
+        double salaryUpdated = employeePayRollService.updateBasicPay(Name, BasicPay);
+        Assert.assertEquals(BasicPay, salaryUpdated,0.0);
+    }
+}
